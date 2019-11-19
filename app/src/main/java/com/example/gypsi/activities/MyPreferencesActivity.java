4284810352559888221bdc27df7710bb5c2eda7a
subha <subhasomya@gmail.com>
2019-11-19 17:46:58 +0530

    preference page added

--- app/src/main/java/com/example/gypsi/activities/MyPreferencesActivity.java
+++ app/src/main/java/com/example/gypsi/activities/MyPreferencesActivity.java
@@ -5,12 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.gypsi.R;
+import com.vivekkaushik.datepicker.DatePickerTimeline;
+import com.vivekkaushik.datepicker.OnDateSelectedListener;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MyPreferencesActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_preferences);
+        DatePickerTimeline datePickerTimeline = findViewById(R.id.dateTimeline);
+// Set a Start date (Default, 1 Jan 1970)
+        datePickerTimeline.setInitialDate(2019, 3, 21);
+// Set a date Selected Listener
+        datePickerTimeline.setOnDateSelectedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(int year, int month, int day, int dayOfWeek) {
+                // Do Something
+            }
+
+            @Override
+            public void onDisabledDateSelected(int year, int month, int day, int dayOfWeek, boolean isDisabled) {
+                // Do Something
+            }
+        });
+
+// Disable date
+        Date[] dates = {Calendar.getInstance().getTime()};
+        datePickerTimeline.deactivateDates(dates);
     }
 }
